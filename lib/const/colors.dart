80d166ada3cf578a7395f1ad4b9eaff31f5e167e
rin213104 <165896180+rin213104@users.noreply.github.com>
2024-05-30 09:42:44 +0900

    Add files via upload

---
+++ lib/const/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xFFD3F3EF);
+const TEXT_COLOR = Color(0xFF6F867C);
---
+++ lib/widget/set_to_do_time.dart
@@ -0,0 +1,370 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tamibara/model/todo_data_model.dart';
+import 'package:tamibara/const/colors.dart';
+
+class SetTime extends StatefulWidget {
+  SetTime({Key? key}) : super(key: key);
+
+  @override
+  State<SetTime> createState() => _SetTimeState();
+}
+
+class _SetTimeState extends State<SetTime> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ToDoDataModel>(
+      builder: (context, toDoData, _) {
+        int selectedDuration = toDoData.selectedDuration;
+        int second = selectedDuration;
+        int hour = second ~/ 3600;
+        second %= 3600;
+        int minute = second ~/ 60;
+        second %= 60;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                '목표 시간',
+                style: TextStyle(
+                  color: TEXT_COLOR,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            SizedBox(height: 2.0),
+            Padding(
+              padding: EdgeInsets.only(left: 5, right: 5),
+              child: Divider(
+                thickness: 1.3,
+                color: TEXT_COLOR,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Expanded(
+              flex: 0,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  showDialog<Duration>(
+                    barrierDismissible: true,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return TimeWheelPicker(
+                        initialTime: Duration(hours: hour, minutes: minute, seconds: second),
+                        onDurationSelected: (newData) {
+                          toDoData.setSelectedDuration(newData.inSeconds);
+                        },
+                      );
+                    },
+                  );
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(top: 14.0, bottom: 14.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${hour}시간',
+                        style: TextStyle(
+                          color: Color(0xFFB1B1B1),
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                      SizedBox(width: 80.0),
+                      Text(
+                        '${minute}분',
+                        style: TextStyle(
+                          color: Color(0xFFB1B1B1),
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                      SizedBox(width: 80.0),
+                      Text(
+                        '${second}초',
+                        style: TextStyle(
+                          color: Color(0xFFB1B1B1),
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class TimeWheelPicker extends StatefulWidget {
+  final Duration initialTime;
+  final ValueChanged<Duration> onDurationSelected;
+
+  TimeWheelPicker({
+    required this.initialTime,
+    required this.onDurationSelected,
+  });
+
+  @override
+  _TimeWheelPickerState createState() => _TimeWheelPickerState();
+}
+
+class _TimeWheelPickerState extends State<TimeWheelPicker> {
+  late int hour;
+  late int minute;
+  late int second;
+
+  @override
+  void initState() {
+    super.initState();
+    hour = widget.initialTime.inHours;
+    minute = widget.initialTime.inMinutes % 60;
+    second = widget.initialTime.inSeconds % 60;
+  }
+
+  List<int> _createList(int length) {
+    return List<int>.generate(length, (index) => index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.4,
+        child: AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          backgroundColor: Colors.white,
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '목표 시간',
+                style: TextStyle(
+                  color: TEXT_COLOR,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              Divider(
+                thickness: 1.3,
+                color: TEXT_COLOR,
+              ),
+            ],
+          ),
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Flexible(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '시간',
+                                    style: TextStyle(
+                                      height: 0.1,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                ],
+                              ),
+                            ),
+                            Flexible(
+                              flex: 8,
+                              child: ListWheelScrollView.useDelegate(
+                                controller: FixedExtentScrollController(initialItem: hour),
+                                overAndUnderCenterOpacity: 0.5,
+                                useMagnifier: true,
+                                itemExtent: 30,
+                                onSelectedItemChanged: (i) {
+                                  setState(() {
+                                    hour = i;
+                                  });
+                                },
+                                childDelegate: ListWheelChildBuilderDelegate(
+                                  builder: (context, index) {
+                                    return Text(index.toString());
+                                  },
+                                  childCount: 24,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Flexible(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '분',
+                                    style: TextStyle(
+                                      height: 0.1,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                ],
+                              ),
+                            ),
+                            Flexible(
+                              flex: 8,
+                              child: ListWheelScrollView.useDelegate(
+                                controller: FixedExtentScrollController(initialItem: minute),
+                                overAndUnderCenterOpacity: 0.5,
+                                useMagnifier: true,
+                                itemExtent: 30,
+                                onSelectedItemChanged: (i) {
+                                  setState(() {
+                                    minute = i;
+                                  });
+                                },
+                                childDelegate: ListWheelChildBuilderDelegate(
+                                  builder: (context, index) {
+                                    return Text(index.toString());
+                                  },
+                                  childCount: 60,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Flexible(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '초',
+                                    style: TextStyle(
+                                      height: 0.1,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                ],
+                              ),
+                            ),
+                            Flexible(
+                              flex: 8,
+                              child: ListWheelScrollView.useDelegate(
+                                controller: FixedExtentScrollController(initialItem: second),
+                                overAndUnderCenterOpacity: 0.5,
+                                useMagnifier: true,
+                                itemExtent: 30,
+                                onSelectedItemChanged: (i) {
+                                  setState(() {
+                                    second = i;
+                                  });
+                                },
+                                childDelegate: ListWheelChildBuilderDelegate(
+                                  builder: (context, index) {
+                                    return Text(index.toString());
+                                  },
+                                  childCount: 60,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            ButtonBar(
+              buttonPadding: EdgeInsets.symmetric(horizontal: 0),
+              children: [
+                SizedBox(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '취소',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20.0),
+                SizedBox(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {
+                      final newData = Duration(hours: hour, minutes: minute, seconds: second);
+                      widget.onDurationSelected(newData);
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '확인',
+                      style: TextStyle(
+                        color: TEXT_COLOR,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
