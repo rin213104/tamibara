9dd49490732e797c71ced9a4680bfb7fcd7a2f19
rin213104 <165896180+rin213104@users.noreply.github.com>
2024-06-02 17:47:58 +0900

    Add files via upload

---
+++ lib/screen/to_do_list_screen.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:timer/model/gaming_data_model.dart';
+import '../widget/to_do_card.dart';
+import '../const/colors.dart';
+import '../screen/add_to_do_screen.dart';
+import '../screen/edit_to_do_screen.dart';
+import '../shared/menu_bottom.dart';
+import 'package:provider/provider.dart';
+import '../model/todo_data_model.dart';
+import 'package:fluttertoast/fluttertoast.dart'; // 토스트 알림을 위해 추가
+import 'dart:math'; // 랜덤 메시지를 위해 추가
+
+class ToDoScreen extends StatefulWidget { //투두 리스트 화면
+  final List<ToDoCard> ToDoList = [];
+
+  ToDoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoScreen> createState() => _setToDoScreenState();
+}
+
+class _setToDoScreenState extends State<ToDoScreen> {
+  final List<String> _congratulatoryMessages = [ // 축하 메시지 리스트 추가
+    "목표를 달성하셨네요!\n정말 대단해요!",
+    "잘했어요!\n꾸준한 모습이 좋아요!",
+    "목표 달성!\n보람이 느껴지나요?",
+    "대단해요!\n앞으로도 계속 이렇게 해봐요!",
+    "목표 달성 완료!\n꾸준히 한다면 더 큰 목표도 이룰 수 있을 거예요!",
+    "정말 잘했어요!\n열심히 하셨네요!",
+    "수고했어요!\n앞으로도 열심히 해봐요!"
+  ];
+
+  /*
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _removePastTodos();
+    });
+  }
+
+   */
+
+  void _removePastTodos() { // 현재 날짜를 기준으로 지난 할 일들을 삭제
+    var GamingData = Provider.of<GamingDataModel>(context, listen: false);
+    //print("removePastTodos,,,,,,,,,,,,,,,,,,,,,");
+
+    DateTime now = DateTime.now();
+    DateTime startOfDay = DateTime(now.year, now.month, now.day);
+    int initialLength = widget.ToDoList.length;
+    setState(() {
+      widget.ToDoList.removeWhere((todo) => todo.Date.isBefore(startOfDay));
+    });
+    int removedCount = initialLength - widget.ToDoList.length;
+
+    for (int i = 0; i < removedCount; i++) {
+      GamingData.decreaseEXP(); // 지난 할 일 삭제 시 삭제된 카드만큼 경험치 -10씩
+    }
+  }
+
+  void _handleToDoChecked(bool isChecked, int index) {
+    if (isChecked) {
+      setState(() {
+        widget.ToDoList.removeAt(index);
+      });
+      // 체크박스 상태 변경 시 랜덤 메시지 출력
+      _showRandomToast();
+      // 체크 박스 상태 변경 시 경험치 증가
+      Provider.of<GamingDataModel>(context, listen: false).increaseCheckedEXP();
+    }
+  }
+
+  void _ToDoDelete(index) {
+    setState(() {
+      widget.ToDoList.removeAt(index);
+    });
+  }
+
+  Future<void> _ToDoEdit(int index) async {
+    Provider.of<ToDoDataModel>(context, listen: false).setSelectedDate(widget.ToDoList[index].Date);
+    Provider.of<ToDoDataModel>(context, listen: false).setSelectedDuration(widget.ToDoList[index].DurationTime);
+    final editResults = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => EditToDo(todo: widget.ToDoList[index])),
+    );
+
+    if (editResults != null) {
+      setState(() {
+        ToDoCard updatedToDo = editResults[0];
+        widget.ToDoList[index] = updatedToDo;
+        widget.ToDoList.sort((a, b) => a.Date.compareTo(b.Date));
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var ToDoData = Provider.of<ToDoDataModel>(context);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _removePastTodos();
+    });
+
+    return Scaffold(
+      appBar: ToDoAppbar(),
+      body: Container(
+        color: PRIMARY_COLOR,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: widget.ToDoList.isEmpty
+              ? SizedBox.expand()
+              : ListView.builder(
+            itemCount: widget.ToDoList.length,
+            itemBuilder: (context, index) {
+              bool showDateDivider = index == 0 ||
+                  widget.ToDoList[index].Date != widget.ToDoList[index - 1].Date;
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  if (showDateDivider) ...[
+                    SizedBox(height: 10),
+                    Text(
+                      '${widget.ToDoList[index].Date.year}.${widget.ToDoList[index].Date.month}.${widget.ToDoList[index].Date.day}',
+                      style: TextStyle(
+                        color: TEXT_COLOR,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Divider(
+                      thickness: 1.3,
+                      color: TEXT_COLOR,
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                  ToDoCard(
+                    Id: widget.ToDoList[index].Id,
+                    Title: widget.ToDoList[index].Title,
+                    Date: widget.ToDoList[index].Date,
+                    DurationTime: widget.ToDoList[index].DurationTime,
+                    Memo: widget.ToDoList[index].Memo,
+                    isChecked: widget.ToDoList[index].isChecked,
+                    onChecked: (isChecked) => _handleToDoChecked(isChecked, index),
+                    onCancel: () => _ToDoDelete(index),
+                    onEdit: () {
+                      _ToDoEdit(index);
+                    },
+                  ),
+                  SizedBox(height: 10),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFFBBD9D6),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(90)),
+        onPressed: () async {
+          ToDoData.setSelectedDate(DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day));
+          ToDoData.setSelectedDuration(0);
+          final results = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddToDo()),
+          );
+
+          if (results != null) {
+            final List<ToDoCard> newToDoList = results;
+            setState(() {
+              widget.ToDoList.addAll(newToDoList);
+              widget.ToDoList.sort((a, b) => a.Date.compareTo(b.Date));
+            });
+          }
+        },
+        child: Icon(Icons.add, color: TEXT_COLOR),
+      ),
+      bottomNavigationBar: MenuBottom(),
+    );
+  }
+
+  // 랜덤 메시지를 표시하는 함수 추가
+  void _showRandomToast() {
+    final random = Random();
+    final message = _congratulatoryMessages[random.nextInt(_congratulatoryMessages.length)];
+    _showToast(message);
+  }
+
+  // 토스트 알림을 위한 함수 추가
+  void _showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 5,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 20.0,
+    );
+  }
+}
+
+AppBar ToDoAppbar() {
+  return AppBar(
+    backgroundColor: PRIMARY_COLOR,
+    title: Text(
+      '목표 리스트',
+      style: TextStyle(
+        color: TEXT_COLOR,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+    centerTitle: false,
+  );
+}
---
+++ lib/widget/to_do_card.dart
@@ -0,0 +1,262 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:timer/const/colors.dart';
+import 'package:timer/model/todo_data_model.dart';
+import 'package:timer/screen/edit_to_do_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:timer/model/todo_data_model.dart';
+
+class ToDoCard extends StatefulWidget {
+  String Id;
+  String Title;
+  DateTime Date;
+  int DurationTime;
+  String Memo;
+  bool isChecked;
+  ValueChanged<bool> onChecked;
+  final Function onCancel;
+  final Function onEdit;
+
+  ToDoCard({
+    required this.Id,
+    required this.Title,
+    required this.Date,
+    required this.DurationTime,
+    required this.Memo,
+    this.isChecked = false,
+    required this.onChecked,
+    required this.onCancel,
+    required this.onEdit,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ToDoCard> createState() => _ToDoCardState();
+}
+
+class _ToDoCardState extends State<ToDoCard> {
+  late bool isChecked;
+
+  String formatted(int x) {
+    return x.toString().padLeft(2, "0");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isChecked = widget.isChecked;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    var ToDoData = Provider.of<ToDoDataModel>(context);
+
+    int second = widget.DurationTime;
+    int hour = second ~/ 3600;
+    second %= 3600;
+    int minute = second ~/ 60;
+    second %= 60;
+
+    return Container( // To-do-list 내용이 담긴 카드
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      height: 70,
+      child: Padding(
+          padding: EdgeInsets.only(left: 5, right: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Checkbox( // 체크박스
+                    value: isChecked,
+                    activeColor: TEXT_COLOR,
+                    checkColor: Colors.white,
+                    onChanged: (value) {
+                      setState(() {
+                        //isChecked = false;
+                        isChecked = value ?? false;
+                      });
+                      widget.onChecked(isChecked);
+                    }),
+              ),
+
+              Flexible(
+                flex: 6,
+                child: Container(
+                  width: 250,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text( // 타이틀
+                        widget.Title,
+                        style: TextStyle(
+                          fontSize: 19,
+                          color: Color(0xFF393939),
+                        ),
+                      ),
+                      Text( //duration
+                        "${formatted(hour)}:${formatted(minute)}:${formatted(second)}",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xFF4D6058)
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Flexible(
+                flex: 1,
+                child: IconButton( // 상세정보
+                  padding: EdgeInsets.zero, // 패딩 설정
+                  constraints: BoxConstraints(), // constraints
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(widget.Title),
+                            content: widget.Memo.isNotEmpty
+                                ? Text(widget.Memo) : Text(""),
+                            actions: <Widget>[
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                  minimumSize: Size.zero,
+                                  padding: EdgeInsets.zero,
+                                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context); // 버튼 클릭 시 팝업 닫기
+                                },
+                                child: Text(
+                                  'OK',
+                                  style: TextStyle(
+                                    color: TEXT_COLOR,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                    );
+                  },
+                  icon: Icon(
+                    Icons.info_outline,
+                    //size: 20,
+                    //color: Color(0xFF6F867C),
+                  ),
+                ),
+              ),
+
+              Flexible(
+                flex: 1,
+                child: IconButton( // 수정
+                  padding: EdgeInsets.zero, // 패딩 설정
+                  constraints: BoxConstraints(),
+                  onPressed: () {
+                    widget.onEdit();
+                  },
+                  icon: Icon(
+                    Icons.edit_outlined,
+                    //color: Color(0xFF6F867C),
+                  ),
+                ),
+              ),
+
+              Flexible(
+                flex: 1,
+                child: IconButton( // 삭제
+                  padding: EdgeInsets.zero, // 패딩 설정
+                  constraints: BoxConstraints(),
+                  onPressed: () {
+                    // 삭제 여부 묻는 다이얼로그 + 카드 삭제 함수
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(
+                              widget.Title,
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            content: Text(
+                              "정말로 삭제하시겠습니까?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                              //textAlign: TextAlign.center,
+                            ),
+                            actions: <Widget>[
+                              ButtonBar(
+                                buttonPadding: EdgeInsets.symmetric(horizontal: 0),
+                                children: [
+                                  SizedBox(
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                        minimumSize: Size.zero,
+                                        padding: EdgeInsets.zero,
+                                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text(
+                                        '취소',
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 20.0),
+                                  SizedBox(
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                        minimumSize: Size.zero,
+                                        padding: EdgeInsets.zero,
+                                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                      ),
+                                      onPressed: () {
+                                        widget.onCancel();
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text(
+                                        '확인',
+                                        style: TextStyle(
+                                          color: TEXT_COLOR,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          );
+                        }
+                    );
+                  },
+                  icon: Icon(
+                    Icons.cancel_outlined,
+                    //color: Color(0xFF6F867C),
+                  ),
+                  //iconSize: 8,
+                ),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
